76566f3cfc505a741d861f342b4dc3d57fef2e71
Hernany Bastos <hernanybastosfig@gmail.com>
2025-01-28 08:45:16 -0300

    "Criação do componente Detalhes do usuários"

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from "@angular/core";
 
+//Components Angular Material
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [
+        MatListModule,
+        MatDividerModule
+    ],
+    exports: [
+        MatListModule,
+        MatDividerModule
+    ]
 })
 export class AngularMaterialModule{}
